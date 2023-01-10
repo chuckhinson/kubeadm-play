3fc93ebab6835189ecc74a7aebba5655d7945594
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2023-01-10 13:54:08 -0500

    Make containerd use cgroup v2

--- packer/setup-containerd.sh
+++ packer/setup-containerd.sh
@@ -45,5 +45,7 @@ sudo apt-get install -y containerd.io
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
 # Restart containerd
 sudo systemctl restart containerd
