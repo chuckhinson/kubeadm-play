d07a2e39daf125b466c5bd59428e458e2c3b19cf
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2023-02-25 15:52:59 -0500

    remove commented-out code

--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,10 +2,6 @@ output "private_subnet_id" {
   value = aws_subnet.private.id
 }
 
-# output "controller_ips" {
-#   value = aws_instance.controllers[*].private_ip
-# }
-
 output "controller_nodes" {
   value = aws_instance.controllers
 }
