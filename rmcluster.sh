5ca11fc28e6cb927299f783691de49bfc1b1f77e
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2023-02-27 22:41:23 -0500

    Add rmcluster.sh to revert kubeadm init This basically deletes the cluster, but doesnt delete the infrastructure

---
+++ rmcluster.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare SSH_CONFIG_FILE="$(pwd)/ssh_config"
+
+declare CONTROLLER_NODES
+declare WORKER_NODES
+
+function gatherClusterInfoFromTerraform () {
+
+  echo "Gathering infrastructure info"
+
+  CONTROLLER_NODES=()
+  while read -r IP; do
+    CONTROLLER_NODES+=("$IP")
+  done <<< "$(terraform -chdir=./terraform output -json | jq -r '.controller_nodes.value')"
+
+  WORKER_NODES=()
+  while read -r IP; do 
+    WORKER_NODES+=("$IP")
+  done <<< "$(terraform -chdir=./terraform output -json | jq -r '.worker_nodes.value')"
+
+}
+
+function resetNodes () {
+
+  local nodes=( "${WORKER_NODES[@]}" "${CONTROLLER_NODES[@]}" )
+
+  for node in "${nodes[@]}"; do
+    echo "resetting node $node"
+    ssh -F "${SSH_CONFIG_FILE}" "$node" "sudo kubeadm reset --force"
+  done
+
+}
+
+
+function main () {
+
+  gatherClusterInfoFromTerraform
+  resetNodes
+
+}
+
+
+main "$@"
